e742b8febc396fa955d90bc1a2383f1993dfef8f
sevan.keshish0 <sevan.keshish0@gmail.com>
2023-07-14 15:53:28 +0400

    adding search

--- src/components/TransactionComponent.jsx
+++ src/components/TransactionComponent.jsx
@@ -1,14 +1,45 @@
+import { useEffect, useState } from "react";
+
 const TransActionComponent = ({transactionsProps}) => {
+    const [searchItem,setSearchItem] = useState("")
+    const [filteredTnx, setFilteredTxn] = useState(transactionsProps)
+
+    const changeHandler = (e) =>{
+        setSearchItem(e.target.value)
+        filterTransactions(e.target.value)
+    }
+
+    const filterTransactions = (search) => {
+        if(!search || search ===""){
+            setFilteredTxn(transactionsProps)
+            return;
+        }
+        const filtered = transactionsProps.filter((t) =>
+         t.desc.toLowerCase().includes(search.toLowerCase())
+        );
+        setFilteredTxn(filtered);
+    }
+
+    useEffect(()=>{
+        filterTransactions(searchItem)
+    },[transactionsProps])
+
+    if(!transactionsProps.length) return <h3>add some transactions</h3>
+
     return ( 
         <section>
-            {transactionsProps.map((t) => (
+            <input type="text" className="search" value={searchItem} onChange={changeHandler} placeholder="search fro transactions ..."/>
+            {filteredTnx.length ? 
+            filteredTnx.map((t) => (
                 <div className="transaction" style={{borderRight: t.type === "expense" && "4px solid red"}} key={t.id}>
                     <span>{t.desc}</span>
                     <span>$ {t.amount}</span>
                 </div>
-            ))}
+            ))
+            : "no item matchs !"
+        }
         </section>
      );
 }
  
-export default TransActionComponent;
+export default TransActionComponent; 
